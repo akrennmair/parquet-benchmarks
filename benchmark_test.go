7f8981bbe3a4132f1e818fcacdf7f09a51f76925
Andreas Krennmair <ak@synflood.at>
2022-03-27 00:39:57 +0100

    Add first benchmark.

---
+++ benchmark_test.go
@@ -0,0 +1,284 @@
+package benchmark_test
+
+import (
+	"os"
+	"testing"
+
+	parquet3 "github.com/apache/arrow/go/v8/parquet"
+	"github.com/apache/arrow/go/v8/parquet/compress"
+	"github.com/apache/arrow/go/v8/parquet/file"
+	"github.com/apache/arrow/go/v8/parquet/schema"
+	goparquet "github.com/fraugster/parquet-go"
+	"github.com/fraugster/parquet-go/floor"
+	"github.com/fraugster/parquet-go/floor/interfaces"
+	"github.com/fraugster/parquet-go/parquet"
+	"github.com/fraugster/parquet-go/parquetschema"
+	parquet2 "github.com/xitongsys/parquet-go/parquet"
+	"github.com/xitongsys/parquet-go/writer"
+)
+
+func BenchmarkIssue84(b *testing.B) {
+	numRecords := 10000
+
+	b.Run("parquet_go_floor_reflection", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			func() {
+				schemaDef, err := parquetschema.ParseSchemaDefinition(
+					`message test {
+						required binary format (STRING);
+						required int32 data_type;
+						required binary country (STRING);
+					}`)
+				if err != nil {
+					b.Fatalf("Parsing schema definition failed: %v", err)
+				}
+
+				parquetFilename := "parquet_go_floor_reflection.parquet"
+
+				fw, err := floor.NewFileWriter(parquetFilename,
+					goparquet.WithSchemaDefinition(schemaDef),
+					goparquet.WithCompressionCodec(parquet.CompressionCodec_SNAPPY),
+				)
+				if err != nil {
+					b.Fatalf("Opening parquet file for writing failed: %v", err)
+				}
+
+				type record struct {
+					Format   string `parquet:"format"`
+					DataType int32  `parquet:"data_type"`
+					Country  string `parquet:"country"`
+				}
+
+				for i := 0; i < numRecords; i++ {
+					stu := record{
+						Format:   "Test",
+						DataType: 1,
+						Country:  "IN",
+					}
+					if err = fw.Write(stu); err != nil {
+						b.Fatalf("Write error: %v", err)
+					}
+				}
+
+				if err := fw.Close(); err != nil {
+					b.Fatalf("Closing parquet writer failed: %v", err)
+				}
+			}()
+		}
+	})
+
+	b.Run("parquet_go_floor_marshalling", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			func() {
+				schemaDef, err := parquetschema.ParseSchemaDefinition(
+					`message test {
+						required binary format (STRING);
+						required int32 data_type;
+						required binary country (STRING);
+					}`)
+				if err != nil {
+					b.Fatalf("Parsing schema definition failed: %v", err)
+				}
+
+				parquetFilename := "parquet_go_floor_marshalling.parquet"
+
+				fw, err := floor.NewFileWriter(parquetFilename,
+					goparquet.WithSchemaDefinition(schemaDef),
+					goparquet.WithCompressionCodec(parquet.CompressionCodec_SNAPPY),
+				)
+				if err != nil {
+					b.Fatalf("Opening parquet file for writing failed: %v", err)
+				}
+
+				for i := 0; i < numRecords; i++ {
+					stu := &marshalRecord{
+						Format:   "Test",
+						DataType: 1,
+						Country:  "IN",
+					}
+					if err = fw.Write(stu); err != nil {
+						b.Fatalf("Write error: %v", err)
+					}
+				}
+
+				if err := fw.Close(); err != nil {
+					b.Fatalf("Closing parquet writer failed: %v", err)
+				}
+			}()
+		}
+	})
+
+	b.Run("parquet_go_lowlevel", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			func() {
+				schemaDef, err := parquetschema.ParseSchemaDefinition(
+					`message test {
+						required binary format (STRING);
+						required int32 data_type;
+						required binary country (STRING);
+					}`)
+				if err != nil {
+					b.Fatalf("Parsing schema definition failed: %v", err)
+				}
+
+				parquetFilename := "parquet_go_lowlevel.parquet"
+
+				w, err := os.OpenFile(parquetFilename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+				if err != nil {
+					b.Fatalf("Opening %s failed: %v", parquetFilename, err)
+				}
+
+				defer w.Close()
+
+				fw := goparquet.NewFileWriter(w, goparquet.WithSchemaDefinition(schemaDef),
+					goparquet.WithCompressionCodec(parquet.CompressionCodec_SNAPPY))
+
+				for i := 0; i < numRecords; i++ {
+					stu := map[string]interface{}{
+						"format":    []byte("Test"),
+						"data_type": int32(1),
+						"country":   []byte("IN"),
+					}
+					if err = fw.AddData(stu); err != nil {
+						b.Fatalf("Write error: %v", err)
+					}
+				}
+
+				if err := fw.Close(); err != nil {
+					b.Fatalf("Closing parquet writer failed: %v", err)
+				}
+			}()
+		}
+	})
+
+	b.Run("xitongsys_parquet_go", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			func() {
+				var err error
+				w, err := os.Create("xitongsys_parquet_go.parquet")
+				if err != nil {
+					b.Fatalf("Can't create local file: %v", err)
+				}
+
+				type record struct {
+					Format   string `parquet:"name=format, type=BYTE_ARRAY, convertedtype=UTF8, encoding=PLAIN_DICTIONARY"`
+					DataType int32  `parquet:"name=data_type, type=INT32, encoding=PLAIN"`
+					Country  string `parquet:"name=country, type=BYTE_ARRAY, convertedtype=UTF8, encoding=PLAIN_DICTIONARY"`
+				}
+
+				//write
+				pw, err := writer.NewParquetWriterFromWriter(w, new(record), 4)
+				if err != nil {
+					b.Fatalf("Can't create parquet writer: %v", err)
+				}
+
+				pw.CompressionType = parquet2.CompressionCodec_SNAPPY
+
+				for i := 0; i < numRecords; i++ {
+					stu := record{
+						Format:   "Test",
+						DataType: 1,
+						Country:  "IN",
+					}
+					if err = pw.Write(stu); err != nil {
+						b.Fatalf("Write error: %v", err)
+					}
+				}
+				if err = pw.WriteStop(); err != nil {
+					b.Fatalf("WriteStop error: %v", err)
+				}
+				w.Close()
+			}()
+		}
+	})
+
+	b.Run("apache_arrow_parquet", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			func() {
+				w, err := os.OpenFile("apache_arrow_parquet.parquet", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+				if err != nil {
+					b.Fatalf("Opening file failed: %v", err)
+				}
+
+				sc, err := schema.NewGroupNode("test", parquet3.Repetitions.Required, schema.FieldList{
+					schema.MustPrimitive(schema.NewPrimitiveNodeLogical("format", parquet3.Repetitions.Required, &schema.StringLogicalType{}, parquet3.Types.ByteArray, 0, 0)),
+					schema.MustPrimitive(schema.NewPrimitiveNode("data_type", parquet3.Repetitions.Required, parquet3.Types.Int32, 0, 0)),
+					schema.MustPrimitive(schema.NewPrimitiveNodeLogical("country", parquet3.Repetitions.Required, &schema.StringLogicalType{}, parquet3.Types.ByteArray, 0, 0)),
+				}, 0)
+
+				pw := file.NewParquetWriter(w, sc, file.WithWriterProps(parquet3.NewWriterProperties(parquet3.WithCompression(compress.Codecs.Snappy))))
+				defer pw.Close()
+
+				rg := pw.AppendRowGroup()
+
+				col, err := rg.NextColumn()
+				if err != nil {
+					b.Fatalf("NextColumn failed: %v", err)
+				}
+
+				formatCol, ok := col.(*file.ByteArrayColumnChunkWriter)
+				if !ok {
+					b.Fatalf("couldn't assert first column which is %T", col)
+				}
+
+				for i := 0; i < numRecords; i++ {
+					if _, err := formatCol.WriteBatch([]parquet3.ByteArray{[]byte("Test")}, nil, nil); err != nil {
+						b.Fatalf("WriteBatch failed: %v", err)
+					}
+				}
+				formatCol.Close()
+
+				col, err = rg.NextColumn()
+				if err != nil {
+					b.Fatalf("NextColumn failed: %v", err)
+				}
+
+				dataTypeCol, ok := col.(*file.Int32ColumnChunkWriter)
+				if !ok {
+					b.Fatalf("couldn't assert second column which is %T", col)
+				}
+
+				for i := 0; i < numRecords; i++ {
+					if _, err := dataTypeCol.WriteBatch([]int32{1}, nil, nil); err != nil {
+						b.Fatalf("WriteBatch failed: %v", err)
+					}
+				}
+
+				dataTypeCol.Close()
+
+				col, err = rg.NextColumn()
+				if err != nil {
+					b.Fatalf("NextColumn failed: %v", err)
+				}
+
+				countryCol, ok := col.(*file.ByteArrayColumnChunkWriter)
+				if !ok {
+					b.Fatalf("couldn't assert third column which is %T", col)
+				}
+
+				for i := 0; i < numRecords; i++ {
+					if _, err := countryCol.WriteBatch([]parquet3.ByteArray{[]byte("IN")}, nil, nil); err != nil {
+						b.Fatalf("WriteBatch failed: %v", err)
+					}
+				}
+
+				countryCol.Close()
+
+				defer rg.Close()
+			}()
+		}
+	})
+}
+
+type marshalRecord struct {
+	Format   string
+	DataType int32
+	Country  string
+}
+
+func (r *marshalRecord) MarshalParquet(obj interfaces.MarshalObject) error {
+	obj.AddField("format").SetByteArray([]byte(r.Format))
+	obj.AddField("data_type").SetInt32(r.DataType)
+	obj.AddField("country").SetByteArray([]byte(r.Country))
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,33 @@
+module github.com/akrennmair/parquet-benchmarks
+
+go 1.18
+
+require (
+	github.com/apache/arrow/go/v8 v8.0.0-20220326174512-08bfd4c68d0e
+	github.com/fraugster/parquet-go v0.10.0
+	github.com/xitongsys/parquet-go v1.6.2
+)
+
+require (
+	github.com/JohnCGriffin/overflow v0.0.0-20211019200055-46fa312c352c // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/apache/arrow/go/arrow v0.0.0-20200730104253-651201b0f516 // indirect
+	github.com/apache/thrift v0.15.0 // indirect
+	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de // indirect
+	github.com/goccy/go-json v0.7.10 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/klauspost/asmfmt v1.3.1 // indirect
+	github.com/klauspost/compress v1.14.2 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/minio/asm2plan9s v0.0.0-20200509001527-cdd76441f9d8 // indirect
+	github.com/minio/c2goasm v0.0.0-20190812172519-36a3d3bbc4f3 // indirect
+	github.com/pierrec/lz4/v4 v4.1.12 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xitongsys/parquet-go-source v0.0.0-20200817004010-026bad9b25d0 // indirect
+	github.com/zeebo/xxh3 v1.0.1 // indirect
+	golang.org/x/exp v0.0.0-20211216164055-b2b84827b756 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20211013180041-c96bc1413d57 // indirect
+	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27 // indirect
+	golang.org/x/tools v0.1.9 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
